20234ad387aa42ffbaa771f51102a9cb64cd09be
ayush-uno <global>
2023-12-03 04:21:41 +0530

    Updated

--- src/navigation/MainBottomNavigator.jsx
+++ src/navigation/MainBottomNavigator.jsx
@@ -17,6 +17,7 @@ import {ScreenHeaderBtn} from '../components';
 import {DrawerActions, useNavigation} from '@react-navigation/native';
 import {ContextPrimary} from '../global/context/context';
 import Tickets from '../screens/Tickets';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 const Tab = createBottomTabNavigator();
 
 const MainBottomNavigator = () => {
@@ -43,6 +44,25 @@ const MainBottomNavigator = () => {
       keyboardDidHideListener.remove();
     };
   }, []);
+
+  const [userRole, setUserRole] = useState('');
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const role = await AsyncStorage.getItem('ROLE');
+        console.log({role});
+        setUserRole(role);
+      } catch (error) {
+        console.error('Error fetching user role:', error);
+      }
+    };
+
+    fetchUserRole();
+  }, []);
+
+  console.log({userRole});
+
   return (
     <Tab.Navigator
       screenOptions={{
@@ -97,161 +117,335 @@ const MainBottomNavigator = () => {
           title: '',
         }}
       />
-      <Tab.Screen
-        name="Tickets"
-        component={Tickets}
-        listeners={{
-          tabPress: e => {
-            // Prevent default action
-            e.preventDefault();
-          },
-        }}
-        options={{
-          headerStyle: {backgroundColor: COLORS.lightWhite},
-          headerShadowVisible: false,
-          headerTitleAlign: 'center',
-          tabBarIcon: ({color, focused}) => (
-            <Image
-              source={focused ? icons.ticketActive : icons.ticket}
-              style={{width: 25, height: 30}}
-              tintColor={'gray'}
-            />
-          ),
-          headerLeft: () => (
-            <View
-              style={{
-                paddingLeft: SIZES.medium,
-              }}>
-              <ScreenHeaderBtn
-                iconUrl={icons.squareLeftIcon}
-                dimension="80%"
-                handlePress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </View>
-          ),
-          headerTitle: 'Tickets',
-          title: '',
-        }}
-      />
-      <Tab.Screen
-        name="Add"
-        component={Add}
-        options={{
-          headerStyle: {backgroundColor: COLORS.lightWhite},
-          headerShadowVisible: false,
-          headerTitleAlign: 'center',
-          tabBarIcon: ({color, focused}) => (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('Add');
-              }}>
-              <View
-                style={{
-                  backgroundColor: '#000000',
-                  width: 60,
-                  height: 60,
-                  borderRadius: 30,
-                  marginBottom: keyboardOpen ? -15 : 30,
-                  elevation: 1,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Image
-                  source={icons.addIcon}
-                  style={{
-                    height: 25,
-                    width: 25,
-                  }}
-                  tintColor={'#FFFFFF'}
-                />
-              </View>
-            </TouchableOpacity>
-          ),
-          headerLeft: () => (
-            <View
-              style={{
-                paddingLeft: SIZES.medium,
-              }}>
-              <ScreenHeaderBtn
-                iconUrl={icons.squareLeftIcon}
-                dimension="80%"
-                handlePress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </View>
-          ),
-          headerTitle: 'Add',
-          title: '',
-        }}
-      />
-      <Tab.Screen
-        name="Visitors"
-        component={Visitors}
-        options={{
-          headerStyle: {backgroundColor: COLORS.lightWhite},
-          headerShadowVisible: false,
-          headerTitleAlign: 'center',
-          tabBarIcon: ({color, focused}) => (
-            <Image
-              source={
-                focused ? icons.peopleGroupIconActive : icons.peopleGroupIcon
-              }
-              style={{width: 25, height: 30}}
-            />
-          ),
-          headerLeft: () => (
-            <View
-              style={{
-                paddingLeft: SIZES.medium,
-              }}>
-              <ScreenHeaderBtn
-                iconUrl={icons.squareLeftIcon}
-                dimension="80%"
-                handlePress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </View>
-          ),
 
-          headerTitle: 'Visitors',
-          title: '',
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          headerStyle: {backgroundColor: COLORS.lightWhite},
-          headerShadowVisible: false,
-          headerTitleAlign: 'center',
-          tabBarIcon: ({color, focused}) => (
-            <Image
-              source={focused ? icons.profileIconActive : icons.profileIcon}
-              style={{width: 25, height: 30}}
-            />
-          ),
-          headerLeft: () => (
-            <View
-              style={{
-                paddingLeft: SIZES.medium,
-              }}>
-              <ScreenHeaderBtn
-                iconUrl={icons.squareLeftIcon}
-                dimension="80%"
-                handlePress={() => {
-                  navigation.goBack();
-                }}
-              />
-            </View>
-          ),
-          headerTitle: 'Profile',
-          title: '',
-        }}
-      />
+      {userRole === 'SECURITY_PERSONNEL' && (
+        <>
+          <Tab.Screen
+            name="Tickets"
+            component={Tickets}
+            listeners={{
+              tabPress: e => {
+                // Prevent default action
+                e.preventDefault();
+              },
+            }}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <Image
+                  source={focused ? icons.ticketActive : icons.ticket}
+                  style={{width: 25, height: 30}}
+                  tintColor={'gray'}
+                />
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+              headerTitle: 'Tickets',
+              title: '',
+            }}
+          />
+          <Tab.Screen
+            name="Add"
+            component={Add}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('Add');
+                  }}>
+                  <View
+                    style={{
+                      backgroundColor: '#000000',
+                      width: 60,
+                      height: 60,
+                      borderRadius: 30,
+                      marginBottom: keyboardOpen ? -15 : 30,
+                      elevation: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Image
+                      source={icons.addIcon}
+                      style={{
+                        height: 25,
+                        width: 25,
+                      }}
+                      tintColor={'#FFFFFF'}
+                    />
+                  </View>
+                </TouchableOpacity>
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+              headerTitle: 'Add',
+              title: '',
+            }}
+          />
+          <Tab.Screen
+            name="Visitors"
+            component={Visitors}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <Image
+                  source={
+                    focused
+                      ? icons.peopleGroupIconActive
+                      : icons.peopleGroupIcon
+                  }
+                  style={{width: 25, height: 30}}
+                />
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+
+              headerTitle: 'Visitors',
+              title: '',
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <Image
+                  source={focused ? icons.profileIconActive : icons.profileIcon}
+                  style={{width: 25, height: 30}}
+                />
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+              headerTitle: 'Profile',
+              title: '',
+            }}
+          />
+        </>
+      )}
+      {userRole === 'SECURITY_APPROVER' && (
+        <>
+          <Tab.Screen
+            name="Tickets"
+            component={Tickets}
+            listeners={{
+              tabPress: e => {
+                // Prevent default action
+                e.preventDefault();
+              },
+            }}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <Image
+                  source={focused ? icons.ticketActive : icons.ticket}
+                  style={{width: 25, height: 30}}
+                  tintColor={'gray'}
+                />
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+              headerTitle: 'Tickets',
+              title: '',
+            }}
+          />
+          <Tab.Screen
+            name="Add"
+            component={Add}
+            listeners={{
+              tabPress: e => {
+                // Prevent default action
+                e.preventDefault();
+              },
+            }}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('Add');
+                  }}>
+                  <View
+                    style={{
+                      backgroundColor: '#000000',
+                      width: 60,
+                      height: 60,
+                      borderRadius: 30,
+                      marginBottom: keyboardOpen ? -15 : 30,
+                      elevation: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Image
+                      source={icons.addIcon}
+                      style={{
+                        height: 25,
+                        width: 25,
+                      }}
+                      tintColor={'#FFFFFF'}
+                    />
+                  </View>
+                </TouchableOpacity>
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+              headerTitle: 'Add',
+              title: '',
+            }}
+          />
+          <Tab.Screen
+            name="Visitors"
+            component={Visitors}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <Image
+                  source={
+                    focused
+                      ? icons.peopleGroupIconActive
+                      : icons.peopleGroupIcon
+                  }
+                  style={{width: 25, height: 30}}
+                />
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+
+              headerTitle: 'Visitors',
+              title: '',
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              headerStyle: {backgroundColor: COLORS.lightWhite},
+              headerShadowVisible: false,
+              headerTitleAlign: 'center',
+              tabBarIcon: ({color, focused}) => (
+                <Image
+                  source={focused ? icons.profileIconActive : icons.profileIcon}
+                  style={{width: 25, height: 30}}
+                />
+              ),
+              headerLeft: () => (
+                <View
+                  style={{
+                    paddingLeft: SIZES.medium,
+                  }}>
+                  <ScreenHeaderBtn
+                    iconUrl={icons.squareLeftIcon}
+                    dimension="80%"
+                    handlePress={() => {
+                      navigation.goBack();
+                    }}
+                  />
+                </View>
+              ),
+              headerTitle: 'Profile',
+              title: '',
+            }}
+          />
+        </>
+      )}
     </Tab.Navigator>
   );
 };
